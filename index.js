913794599ebe3c1f62c721cb31236acc74853aed
liamH47 <liamhanafee@gmail.com>
2021-03-11 15:08:13 -0500

    plswork

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/jobs', jobRoutes);
-
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+})
 const PORT = process.env.PORT || 5000;
 
 mongoose.set('useFindAndModify', false);
